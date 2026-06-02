1b352274521752feaae11bb111d7bdd908988c44
Hari Kaku <harikaku@gmail.com>
2026-06-02 02:22:52 +0000

    add changes 1 June

---
+++ models/cleansed/customer_full_refresh.sql
@@ -0,0 +1,15 @@
+{{
+    config(materialized='incremental',schema='cleansed') 
+    }}
+select 
+customer_id,
+first_name,
+last_name,			
+email,		
+replace(phone,'x','') as phone,		
+city,			
+state,			
+country	,		
+created_at	
+from  {{ source('raw','customers') }} 
+
---
+++ models/cleansed/customer_incremental.sql
@@ -0,0 +1,19 @@
+{{
+    config(materialized='incremental',schema='cleansed') 
+    }}
+select 
+customer_id,
+first_name,
+last_name,			
+email,		
+replace(phone,'x','') as phone,		
+city,			
+state,			
+country	,		
+created_at	
+from  {{ source('raw','customers') }} 
+
+{% if is_incremental() %}
+where created_at > 
+(select max(created_at) from {{ this }})
+{% endif %}
